f6b50336f762cdfd27c2a413b1ef5dd2cda0ecb5
augustinLib <2bambitious@gmail.com>
2022-10-31 18:56:40 +0900

    Feat: Add KorBERT sentence similarity

---
+++ sentence_similarity/find_similar.py
@@ -0,0 +1,14 @@
+import argparse
+from korBERT.similarity import get_similarity
+
+
+
+
+def main():
+    text = input("데이터 명세를 입력하세요 : ")
+    get_similarity(text)
+    
+
+if __name__ == '__main__':
+    main()
+    
---
+++ sentence_similarity/korBERT/utils.py
@@ -0,0 +1,98 @@
+import torch
+import numpy as np
+from numpy import dot
+from numpy.linalg import norm
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+def calc_accuracy(X,Y):
+    max_vals, max_indices = torch.max(X, 1)
+    train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
+    return train_acc
+
+
+def data_preprocess(path):
+    raw_data = pd.read_csv(path)
+
+    result = raw_data.loc[:, ["description", "label"]]
+    result.dropna(axis=0, inplace=True)
+
+    items = list(set(result["label"]))
+
+    encoder = LabelEncoder()
+    encoder.fit(items)
+    result["label"] = encoder.transform(result["label"])
+ 
+    data_list = []
+    for sentence, label in zip(result['description'], result['label']):
+        data = []
+        data.append(sentence)
+        data.append(str(label))
+        data_list.append(data)
+
+    return data_list, encoder.classes_
+
+
+
+def new_softmax(a) : 
+    c = np.max(a) 
+    exp_a = np.exp(a-c) 
+    sum_exp_a = np.sum(exp_a)
+    y = (exp_a / sum_exp_a) * 100
+    return np.round(y, 3)
+
+import numpy as np
+import torch
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+            path (str): Path for the checkpoint to be saved to.
+                            Default: 'checkpoint.pt'
+            trace_func (function): trace print function.
+                            Default: print
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+        self.trace_func = trace_func
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
+
+
+def cos_sim(A, B):
+  return dot(A, B)/(norm(A)*norm(B))
