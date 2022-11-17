78ffd0d9296325d13c225926e63bdb1ab766172e
seokii <dev.seokii@gmail.com>
2022-11-17 17:02:17 +0900

    Djnago PostgreSQL connect

--- Data_Planet/settings.py
+++ Data_Planet/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "Data_Planet.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'data_planet_db',
+        'USER': 'root',
+        'PASSWORD' : 'password',
+        'HOST' : 'localhost',
+        'PORT' :'',
     }
 }
 
