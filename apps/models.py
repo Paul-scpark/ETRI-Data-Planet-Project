8e198417336f3550a399e5600dec352764fb5003
seokii <dev.seokii@gmail.com>
2022-11-23 10:09:58 +0900

    feat: Add User Table

--- apps/models.py
+++ apps/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class Test(models.Model):
     name = models.CharField(max_length=80)
     age = models.IntegerField()
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=32, verbose_name='사용자 이름')
+    email = models.EmailField(max_length=128, verbose_name='사용자 이메일')
+    password = models.CharField(max_length=64, verbose_name='비밀번호')
+    is_admin = models.BooleanField(default=False, verbose_name='관리자')
+    create_dt = models.DateField(auto_now_add=True, verbose_name='가입 날짜')
