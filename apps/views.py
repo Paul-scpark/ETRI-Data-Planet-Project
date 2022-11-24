bd8780b4875edb8c5d43430c61762b5cf5496525
Paul-scpark <75621410+Paul-scpark@users.noreply.github.com>
2022-11-24 23:04:19 +0900

    Feat: Add signup feature

--- apps/views.py
+++ apps/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import bcrypt
+from .models import User
+
 # Create your views here.
 
 def main(request):
@@ -70,7 +73,37 @@ def support(request):
     )
 
 def signup(request):
-    return render(
-        request,
-        'apps/signup.html'
-    )
+    if request.method == "GET":
+        return render(request, 'apps/signup.html')
+    
+    elif request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        re_password = request.POST['rePassword']
+        
+        ### 1. 회원가입 과정에서 이상 여부 확인
+        ## 이메일 중복 확인
+        if User.objects.filter(email=email).exists():
+            return HttpResponse(
+                "<script>alert('이미 존재하는 이메일입니다.\\n회원 가입 페이지로 돌아갑니다.');"
+                "location.href='/signup';</script>"
+            )
+        
+        ## 패스워드 일치 여부 확인
+        if password != re_password:
+            return HttpResponse(
+                "<script>alert('비밀번호 불일치!\\n회원 가입 페이지로 돌아갑니다.');"
+                "location.href='/signup';</script>"
+            )
+        
+        ### 2. 패스워드 해쉬 처리
+        password = password.encode('utf8')
+        password_bcrypt = bcrypt.hashpw(password, bcrypt.gensalt()).decode('utf8')
+        
+        ### 3. 위 로직에서 특이사항 없었으면, DB에 데이터 적재
+        User.objects.create(name=name, email=email, password=password_bcrypt).save()
+        return HttpResponse(
+            "<script>alert('회원가입 완료.\\n메인 페이지로 돌아갑니다.');"
+            "location.href='/';</script>"
+        )
