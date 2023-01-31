6876c4073f20fff1443a07119872bfbea8c283de
Paul-scpark <75621410+Paul-scpark@users.noreply.github.com>
2023-02-01 01:06:01 +0900

    Add dumping csv data into db

--- apps/models.py
+++ apps/models.py
@@ -26,16 +26,16 @@ class DataPlatform(models.Model):
     total_num = models.IntegerField(default=0)
 
     class Meta:
-        db_table = 'DataPlatfrom'
+        db_table = 'DataPlatform'
 
 class Data(models.Model):
     data_id = models.AutoField(primary_key=True)
-    data_platform_id = models.ForeignKey("DataPlatform", on_delete=models.CASCADE)
+    data_platform = models.ForeignKey("DataPlatform", on_delete=models.CASCADE)
     title = models.CharField(max_length=512 ,verbose_name='데이터 이름')
-    des = models.CharField(max_length=1024, verbose_name='데이터 설명')
+    des = models.CharField(max_length=10000, verbose_name='데이터 설명')
     url = models.CharField(max_length=1024, verbose_name='데이터 주소')
-    type = models.CharField(max_length=32, verbose_name='데이터 타입')
-    source = models.CharField(max_length=32, verbose_name='source')
+    type = models.CharField(max_length=64, verbose_name='데이터 타입')
+    source = models.CharField(max_length=300, verbose_name='source')
     ori_label = models.CharField(max_length=32, verbose_name='ori_label')
     ori_source = models.CharField(max_length=32, verbose_name='ori_source')
     label = models.CharField(max_length=32, verbose_name='label')
---
+++ scripts/load_data.py
@@ -0,0 +1,49 @@
+from apps.models import DataPlatform, Data
+import pandas as pd
+from io import StringIO
+import psycopg2
+
+def run():
+    db = psycopg2.connect(
+        host='localhost',
+        dbname='data_planet_db',
+        user='root',
+        password='password',
+        port=5432
+    )
+    
+    cursor = db.cursor()
+    
+    ## DataPlatform 테이블의 데이터가 없는 경우에, DB에 Dump
+    cursor.execute('SELECT * FROM "DataPlatform"')
+    if cursor.fetchone() is None:
+        buffer = StringIO()
+        try:
+            data_platform = pd.read_csv('data_platform.csv') # FIXME: data_platform.csv 파일 경로 위치로 수정하기
+            data_platform.to_csv(buffer, index=True, header=False)
+            buffer.seek(0)
+            cursor.copy_from(buffer, 'DataPlatform', sep=',')
+            db.commit()
+        except (Exception, psycopg2.DatabaseError) as e:
+            print(f"Error: {e}")
+            db.rollback()
+            cursor.close()
+            return 1
+        print("copy_from_stringio() done - DataPlatform")
+        
+    ## Data 테이블의 데이터가 없는 경우에, DB에 Dump
+    cursor.execute('SELECT * FROM "Data"')
+    if cursor.fetchone() is None:
+        buffer = StringIO()
+        try:
+            data_platform = pd.read_csv('total_data.csv', lineterminator='\n') # FIXME: total_data.csv 파일 경로 위치로 수정하기
+            data_platform.to_csv(buffer, index=True, header=False)
+            buffer.seek(0)
+            cursor.copy_from(buffer, 'Data', sep=',')
+            db.commit()
+        except (Exception, psycopg2.DatabaseError) as e:
+            print(f"Error: {e}")
+            db.rollback()
+            cursor.close()
+            return 1
+        print("copy_from_stringio() done - Data")
