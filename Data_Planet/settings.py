a930b1e075dd75b36026507a35a0f20520d97a72
seokii <dev.seokii@gmail.com>
2023-02-07 22:31:41 +0900

    Add: feat free domain address

--- Data_Planet/settings.py
+++ Data_Planet/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '59.18.141.225']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '59.18.141.225', '.data-planet.kro.kr']
 
 
 # Application definition
